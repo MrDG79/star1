1022c4345efb5bb4a7744e4120c6b40cd3b4223c
chochul12 <50858563+chochul12@users.noreply.github.com>
2019-05-21 15:50:02 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1,14 +1,46 @@
 # -*- coding: utf-8 -*- 
 
-#import sys
-#import asyncio
+import sys
+import asyncio
 import discord
-#import datetime
-#import random
-import os
-#from discord.ext import commands
-#from gtts import gTTS
+import datetime
+import random
+from discord.ext import commands
+from gtts import gTTS
+#from discord.ext.commands import Bot
+#from discord.voice_client import VoiceClient
 
+if not discord.opus.is_loaded():
+	discord.opus.load_opus('opus')
+
+basicSetting = []
+bossData = []
+
+bossNum = 0
+chkvoicechannel = 0
+chkrelogin = 0
+chflg = 0
+LoadChk = 0
+
+bossTime = []
+tmp_bossTime = []
+
+bossTimeString = []
+bossDateString = []
+tmp_bossTimeString = []
+tmp_bossDateString = []
+
+bossFlag = []
+bossFlag0 = []
+bossMungFlag = []
+bossMungCnt = []
+
+channel_info = []
+channel_name = []
+channel_id = []
+channel_voice_name = []
+channel_voice_id = []
+channel_type = []
 client = discord.Client()
 
 @client.event
