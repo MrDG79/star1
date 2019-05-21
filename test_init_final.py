50f26442ff5f195c73de47388fe6352ed07863a4
chochul12 <50858563+chochul12@users.noreply.github.com>
2019-05-21 15:45:52 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -9,8 +9,10 @@ import os
 #from discord.ext import commands
 #from gtts import gTTS
 
+client = discord.Client()
+
 @client.event
-async def on_ready:
+async def on_ready():
 	print ("login")
 	print (client.user.name)
 	print (client.user.id)
@@ -21,6 +23,6 @@ async def on_message(message):
 	if message.content.startswith("hi"):
 		await client.send_message(message.channel, "HI")
 
-			
+
 access_token = os.environ["BOT_TOKEN"]			
 client.run(access_token)
