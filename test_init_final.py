286fe95342cf208be9091aa2727fcc275f9e8c29
chochul12 <50858563+chochul12@users.noreply.github.com>
2019-06-06 08:53:27 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -831,7 +831,7 @@ async def on_message(msg):
 				if bossTimeString[i] == '99:99:99' :
 					temp_bossTime2.append(bossData[i][0])
 					
-			for i in range(len(temp_bossTime2))
+			for i in range(len(temp_bossTime2)):
 				temp_bossTimeSTR += temp_bossTime2[i]
 					
 			embed = discord.Embed(
